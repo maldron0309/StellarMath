f28e15d1d83e7914f66eab4c81ea2e4a2720096a
maldron0309 <bagjinhyeong640@gmail.com>
2024-07-29 19:53:43 +0900

    matrix create, destroy

--- StellarMath/Matrix.h
+++ StellarMath/Matrix.h
@@ -1,7 +1,33 @@
 #pragma once
-
-typedef struct Matrix
+#include <stdlib.h>
+#include <string.h>
+#ifdef __cplusplus
+extern "C"
 {
-	int rows;
-	int cols;
-}Matrix;
+#endif
+
+	typedef struct Matrix
+	{
+		int rows;
+		int cols;
+		float* data;
+	}Matrix;
+
+	Matrix MatrixCreate(int rows, int cols)
+	{
+		Matrix m;
+		m.rows = rows;
+		m.cols = cols;
+		m.data = (float*)malloc(rows * cols * sizeof(float));
+		memset(m.data, 0, rows * cols * sizeof(float));
+		return m;
+	}
+
+	void MatrixDestory(Matrix* m)
+	{
+		free(m->data);
+		m->data = NULL;
+		m->rows = 0;
+		m->cols = 0;
+	}
+}
